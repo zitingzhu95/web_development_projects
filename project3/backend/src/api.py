6ad774ca09a2e01eee2ab5290ab975ef7e784dc6
zitingzhu95 <101026850+zitingzhu95@users.noreply.github.com>
2022-07-14 10:36:46 -0700

    backend to done but problem front end from mac mini

--- project3/backend/src/api.py
+++ project3/backend/src/api.py
@@ -4,8 +4,8 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from database.models import db_drop_and_create_all, setup_db, Drink
-from auth.auth import AuthError, requires_auth
+from .database.models import db_drop_and_create_all, setup_db, Drink
+from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
@@ -34,6 +34,30 @@ def getErrorMessage(error, default_text):
         # otherwise, return given default text
         return default_text
 
+def get_all_drinks(recipe_format):
+    '''Queries a formatted list of drinks with long or short recipe description
+    *Input:
+        <string> recipe_format "long" or "short", depending on how detailled the informations are needed
+    *Output:
+        <list> Formatted instances of Drinks
+    If no drinks could be found, abort with 404 error.
+    '''
+    # Get all drinks in database
+    all_drinks = Drink.query.order_by(Drink.id).all()
+    # Format with different recipe detail level
+    if recipe_format.lower() == 'short':
+        all_drinks_formatted = [drink.short() for drink in all_drinks]
+    elif recipe_format.lower() == 'long':
+        all_drinks_formatted = [drink.long() for drink in all_drinks]
+    else:
+        return abort(500, {'message': 'bad formatted function call. recipe_format needs to be "short" or "long".'})
+
+    if len(all_drinks_formatted) == 0:
+        abort(404, {'message': 'no drinks found in database.'})
+    
+    # Return formatted list of drinks
+    return all_drinks_formatted
+
 # ROUTES
 '''
 @TODO implement endpoint
@@ -43,6 +67,13 @@ def getErrorMessage(error, default_text):
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks' , methods=['GET'])
+def drinks():
+    """Returns all drinks"""
+    return jsonify({
+    'success': True,
+    'drinks': get_all_drinks('short')
+    })
 
 
 '''
@@ -53,7 +84,14 @@ def getErrorMessage(error, default_text):
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks-detail',  methods=['GET'])
+@requires_auth('get:drinks-detail')
+def drinks_detail(payload):
+    """Returns all drinks with detailed recipe information"""
+    return jsonify({
+    'success': True,
+    'drinks': get_all_drinks('long')
+    })
 
 '''
 @TODO implement endpoint
@@ -64,7 +102,20 @@ def getErrorMessage(error, default_text):
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks',  methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(payload):
+    """Creates new drink and returns it to client"""
+    
+    body = request.get_json()
+    new_drink = Drink(title = body['title'], recipe = """{}""".format(body['recipe']))
+    
+    new_drink.insert()
+    new_drink.recipe = body['recipe']
+    return jsonify({
+    'success': True,
+    'drinks': Drink.long(new_drink)
+    })
 
 '''
 @TODO implement endpoint
@@ -77,6 +128,38 @@ def getErrorMessage(error, default_text):
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>',  methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(payload, drink_id):
+    """Updates existing drink and returns it to client"""
+    
+    # Get body from request
+    body = request.get_json()
+
+    if not body:
+      abort(400, {'message': 'request does not contain a valid JSON body.'})
+    
+    # Find drink which should be updated by id
+    drink_to_update = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+    # Check if and which fields should be updated
+    updated_title = body.get('title', None)
+    updated_recipe = body.get('recipe', None)
+    
+    # Depending on which fields are available, make apropiate updates
+    if updated_title:
+        drink_to_update.title = body['title']
+    
+    if updated_recipe:
+        drink_to_update.recipe = """{}""".format(body['recipe'])
+    
+    drink_to_update.update()
+
+    return jsonify({
+    'success': True,
+    'drinks': [Drink.long(drink_to_update)]
+    })
+
 
 
 '''
@@ -89,6 +172,25 @@ def getErrorMessage(error, default_text):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>',  methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drinks(payload, drink_id):
+    """Deletes 1 drink with given id"""
+    if not drink_id:
+        abort(422, {'message': 'Please provide valid drink id'})
+
+    # Get drink with id
+    drink_to_delete = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+    if not drink_to_delete:
+        abort(404, {'message': 'Drink with id {} not found in database.'.format(drink_id)})
+     
+    drink_to_delete.delete()
+    
+    return jsonify({
+    'success': True,
+    'delete': drink_id
+    })
 
 
 #----------------------------------------------------------------------------#
